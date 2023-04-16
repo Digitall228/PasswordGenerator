0056bfeba6bfd96696732506561986fe2524bd40
Digitall228 <dia_odessa5@ukr.net>
2023-04-16 01:12:28 +0100

    Add project files.

---
+++ PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PassGen
+{
+    public static class PasswordGenerator
+    {
+        public static Random rnd = new Random();
+        public static string keys = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789";
+        public static string Generate(int number = 20, string Keys = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789")
+        {
+            char[] key = Keys.ToCharArray();
+            string password = string.Empty;
+            
+            for (int i = 0; i < number; i++)
+            {
+                password += key[rnd.Next(0, key.Length)];
+            }
+            return password;
+        }
+    }
+}
